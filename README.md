e95d74c815ae7c2f57b43d5ecedc46f72f12ebc2
raghavsharma007 <princeraghav.sharma@gmail.com>
2020-08-12 15:58:16 +0530

    react + tailwind boilerplate

---
+++ README.md
@@ -0,0 +1,6 @@
+ReactJS + Tailwindcss boilerplate
+1. Clone the repo
+2. cd into project and run "npm install"
+3. npm start to run development server
+4. Tailwindcss configured to build at every npm start
+5. Enjoy Utility classes by Tailwind
---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <div className="flex bg-blue-400 justify-center items-center min-h-screen">
+      <div className="bg-blue-700 rounded-lg p-8 text-gray-400 uppercase font-semibold shadow-2xl text-lg">
+      <svg viewBox="0 0 20 20" fill="currentColor" className="cake h-20 text-yellow-600 m-auto"><path fillRule="evenodd" d="M6 3a1 1 0 011-1h.01a1 1 0 010 2H7a1 1 0 01-1-1zm2 3a1 1 0 00-2 0v1a2 2 0 00-2 2v1a2 2 0 00-2 2v.683a3.7 3.7 0 011.055.485 1.704 1.704 0 001.89 0 3.704 3.704 0 014.11 0 1.704 1.704 0 001.89 0 3.704 3.704 0 014.11 0 1.704 1.704 0 001.89 0A3.7 3.7 0 0118 12.683V12a2 2 0 00-2-2V9a2 2 0 00-2-2V6a1 1 0 10-2 0v1h-1V6a1 1 0 10-2 0v1H8V6zm10 8.868a3.704 3.704 0 01-4.055-.036 1.704 1.704 0 00-1.89 0 3.704 3.704 0 01-4.11 0 1.704 1.704 0 00-1.89 0A3.704 3.704 0 012 14.868V17a1 1 0 001 1h14a1 1 0 001-1v-2.132zM9 3a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1zm3 0a1 1 0 011-1h.01a1 1 0 110 2H13a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+        ReactJS + TailwindCSS
+        </div>
+    </div>
+  );
+}
+
+export default App;
